5321d447d2eba63dee2a49d54b58b57cd2e201bf
Øyvind Skaaden <oyvindskaaden@gmail.com>
2020-04-04 17:41:58 +0200

    Created class for cable tester

--- cabletester.cpp
+++ cabletester.cpp
@@ -1,26 +1,53 @@
 #include "cabletester.h"
 
-///
-/// Converts from a binary number to digital pins between startPin and endPin
-///
-void WriteBits(const uint8_t data, const uint8_t startPin, const uint8_t noPins)
+Cabletester::Cabletester(const array<uint8_t,36>& inputPins, 
+							const array<uint8_t,5>& muxSel, 
+							const array<uint8_t,3>&  muxCtrl) : 
+								inputPins{inputPins},
+								muxSel{muxSel},
+								muxCtrl{muxCtrl}
 {
-	for (uint8_t i = 0; i <= noPins; i++)
+	for (auto p : inputPins)
 	{
-		digitalWrite(i, (data >> i) & 0x1);
+		pinMode(p, INPUT);
+	}
+	for (auto p : muxSel)
+	{
+		pinMode(p, OUTPUT);
+	}
+	for (auto p : muxCtrl)
+	{
+		pinMode(p, OUTPUT);
+	}
+	
+}
+
+Cabletester::~Cabletester()
+{
+}
+
+///
+/// Converts from a binary number data to digital pins in array ctrlPins
+///
+void Cabletester::WriteBits(uint8_t data, const array<uint8_t,3>& ctrlPins)
+{
+	for (const auto p : ctrlPins)
+	{
+		digitalWrite(p, (data) & 0x1);
+		data = data >> 1;
 	}
 }
 
 ///
-/// Reads the pins between startPin and endPin, returns a uint64_t with where the trigger happened.
+/// Reads the pins in the array pins, returns a uint64_t with where the trigger happened.
 /// The LSB is the last input to be scanned, so if you scan pin 1 and 2, the read would be 0b(pin1)(pin2)
 ///
-uint64_t ReadInputs(const uint8_t startPin, const uint8_t noPins)
+uint64_t Cabletester::ReadInputs(const array<uint8_t,36>& pins)
 {
 	uint64_t inputPins = 0x0;
-	for (uint8_t p = 0; p < noPins; p++)
+	for (const auto p : pins)
 	{
-		inputPins = (inputPins + digitalRead(startPin + p)) << 1;
+		inputPins = (inputPins + digitalRead(p)) << 1;
 	}
 	return inputPins;
 }
@@ -28,16 +55,15 @@ uint64_t ReadInputs(const uint8_t startPin, const uint8_t noPins)
 /// 
 /// Returns true if the zero indexed curPin is high, and only that pin
 ///
-boolean CheckIfNormal(const uint64_t& data, const uint8_t curPin)
+boolean Cabletester::CheckIfNormal(const uint64_t& data, const uint8_t curPin)
 {
-	if ((0x1 << curPin) == data)
-		return true;
+	return ((0x1 << curPin) == data);
 }
 
 ///
 /// Returns the number of set bits
 ///
-uint8_t CountSetBits(uint64_t data) 
+uint8_t Cabletester::CountSetBits(uint64_t data) 
 { 
     uint8_t count = 0; 
     while (data) { 
@@ -51,7 +77,7 @@ uint8_t CountSetBits(uint64_t data)
 /// 
 /// Takes in the dataint and finds all the indexes for errors, excludes the current pin
 ///
-uint8_t* FindErrors(uint64_t data, const uint8_t noPin, const uint8_t curPin)
+uint8_t* Cabletester::FindErrors(uint64_t data, const uint8_t noPin, const uint8_t curPin)
 {
 	data = (data & ~(0x1 << curPin)); 	// Removes the currently searched pin
 	uint8_t errors[noPin]; 	// Creates a list to store errors
@@ -76,7 +102,7 @@ uint8_t* FindErrors(uint64_t data, const uint8_t noPin, const uint8_t curPin)
 /// Optional is how long it should scan the pins for, default for delay is 0ms (at least one time).
 /// Optional is to specify how many times the scan should be done, default for count is 1. 
 ///
-uint64_t* ScanPins(const uint8_t muxSelStart, const uint8_t muxSelNo, const uint8_t muxCtrlStart, const uint8_t muxCtrlNo, const uint8_t muxStates, const uint8_t inputStart, const uint8_t inputNo, const uint16_t delay = 0, const uint8_t count = 1)
+uint64_t* Cabletester::ScanPins(const uint8_t muxSelStart, const uint8_t muxSelNo, const uint8_t muxCtrlStart, const uint8_t muxCtrlNo, const uint8_t muxStates, const uint8_t inputStart, const uint8_t inputNo, const uint16_t delay = 0, const uint8_t count = 1)
 {
 	uint8_t selected = 0x1;
 	uint64_t results[inputNo];
--- cabletester.h
+++ cabletester.h
@@ -1,6 +1,10 @@
 #ifndef cabletester
 #define cabletester
 
+#include <array>
+
+using namespace std;
+
 // LK pinout and configuration
 uint8_t noPairs = 12;
 char lkConfig[] = {'+', '-', 'G'};
@@ -20,40 +24,64 @@ char lkPinout[] =
 	'r', 's', 'k', 
 };
 
-///
-/// Converts from a binary number to digital pins between startPin and endPin
-///
-void WriteBits(const uint8_t data, const uint8_t startPin, const uint8_t noPins);
+class Cabletester
+{
+private:
+	array<uint64_t, 36> results;
 
-///
-/// Reads the pins between startPin and endPin, returns a uint64_t with where the trigger happened.
-/// The LSB is the last input to be scanned, so if you scan pin 1 and 2, the read would be 0b(pin1)(pin2)
-///
-uint64_t ReadInputs(const uint8_t startPin, const uint8_t noPins);
+	const array<uint8_t,36>& inputPins;
 
-/// 
-/// Returns true if the zero indexed curPin is high, and only that pin
-///
-boolean CheckIfNormal(const uint64_t& data, const uint8_t curPin);
+	const array<uint8_t,5>& muxSel;
 
-///
-/// Returns the number of set bits
-///
-uint8_t CountSetBits(uint64_t data);
+	const array<uint8_t,3>& muxCtrl;
+
+public:
+	Cabletester(const array<uint8_t,36>& inputPins, 
+				const array<uint8_t,5>& muxSel, 
+				const array<uint8_t,3>&  muxCtrl);
+
+	~Cabletester();
+
+	///
+	/// Converts from a binary number data to digital pins in array ctrlPins
+	///
+	void WriteBits(uint8_t data, const const array<uint8_t,3>& ctrlPins);
+
+	///
+	/// Reads the pins between startPin and endPin, returns a uint64_t with where the trigger happened.
+	/// The LSB is the last input to be scanned, so if you scan pin 1 and 2, the read would be 0b(pin1)(pin2)
+	///
+	uint64_t ReadInputs(const array<uint8_t,36>& pins);
+
+	/// 
+	/// Returns true if the zero indexed curPin is high, and only that pin
+	///
+	boolean CheckIfNormal(const uint64_t& data, const uint8_t curPin);
+
+	///
+	/// Returns the number of set bits
+	///
+	uint8_t CountSetBits(uint64_t data);
 
 
-/// 
-/// Takes in the dataint and finds all the indexes for errors, excludes the current pin
-///
-uint8_t* FindErrors(uint64_t data, const uint8_t noPin, const uint8_t curPin);
+	/// 
+	/// Takes in the dataint and finds all the indexes for errors, excludes the current pin
+	///
+	uint8_t* FindErrors(uint64_t data, const uint8_t noPin, const uint8_t curPin);
 
 
-///
-/// Scans all the pins specified by the arguments, specify where the MUXsel pins start and amount, where MUXctrl are and the number of states, input start and number.
-/// Returns a list of length inputNo (up to 64 inputs), containing all the scan data.
-/// Optional is how long it should scan the pins for, default for delay is 0ms (at least one time).
-/// Optional is to specify how many times the scan should be done, default for count is 1. 
-///
-uint64_t* ScanPins(const uint8_t muxSelStart, const uint8_t muxSelNo, const uint8_t muxCtrlStart, const uint8_t muxCtrlNo, const uint8_t muxStates, const uint8_t inputStart, const uint8_t inputNo, const uint16_t delay = 0, const uint8_t count = 1);
+	///
+	/// Scans all the pins specified by the arguments, specify where the MUXsel pins start and amount, where MUXctrl are and the number of states, input start and number.
+	/// Returns a list of length inputNo (up to 64 inputs), containing all the scan data.
+	/// Optional is how long it should scan the pins for, default for delay is 0ms (at least one time).
+	/// Optional is to specify how many times the scan should be done, default for count is 1. 
+	///
+	uint64_t* ScanPins(const uint8_t muxSelStart, const uint8_t muxSelNo, const uint8_t muxCtrlStart, const uint8_t muxCtrlNo, const uint8_t muxStates, const uint8_t inputStart, const uint8_t inputNo, const uint16_t delay = 0, const uint8_t count = 1);
+};
+
+
+
+
+
 
 #endif
